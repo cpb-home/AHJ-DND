9c59ba25b254ee02faefd0f0d67704fb43b97a02
cpb-home <cpb@bizb.ru>
2024-05-05 12:21:57 +0300

    phantom done

--- src/js/classes/Column/Column.js
+++ src/js/classes/Column/Column.js
@@ -52,7 +52,7 @@ export default class Column {
       const card = this.createCard(i);
 
       cardList.append(card);
-      this.cardDragger(card);
+      this.cardDragger();
     }
     if (this.cards.length === 0) {
       cardList.classList.add('bordered');
@@ -199,9 +199,8 @@ export default class Column {
     } 
   }
 
-  cardDragger(card) {
-    let dragged = null;
-
+  cardDragger() {
+    let dragged = undefined;
     const isUpperThenCenter = (e) => {
       const currentElementCoord = e.target.getBoundingClientRect();
       const currentElementCenter = currentElementCoord.y + currentElementCoord.height / 2;
@@ -211,14 +210,17 @@ export default class Column {
       } 
       return false;
     };
+    const delPhantom = () => {
+      if (document.querySelector('.phantom')) {
+        document.querySelector('.phantom').remove();
+      }
+    }
 
-    //const source = card.closest(".cardList");
-    const source = document.querySelector('#root');
-    source.addEventListener("dragstart", e => {
+    const cont = document.querySelector('#root');
+    cont.addEventListener("dragstart", e => {
       dragged = e.target;
     });
 
-    //const target = card.closest(".cardList");
     const target = document.querySelector('#root');
     target.addEventListener("dragover", (e) => {
       e.preventDefault();
@@ -227,54 +229,167 @@ export default class Column {
       const width = dragged.offsetWidth;
       const phantom = document.createElement('div');
       phantom.classList.add('phantom');
-      phantom.height = height;
-      phantom.width = width;
+      phantom.style.height = height + 'px';
+      phantom.style.width = width + 'px';
+      
 
-      if ()
 
-      if (e.target.classList.contains('card')) {
-        if (target.querySelector('.phantom')) {
-          
-        } else {
 
+
+      
+
+// не перемещается фантом
+      let current;
+      if (!(e.target.classList.contains('card') || e.target.classList.contains('cardList' || e.target.classList.contains('phantom')))) {
+        //delPhantom();
+        return;
+      } 
+
+      if (!document.querySelector('.phantom')) {                // если фантома нет 
+        current = e.target;
+        if (e.target.classList.contains('card')) {                // если мы над карточкой
+          if (isUpperThenCenter(e)) {
+            e.target.before(phantom);
+          } else {
+            e.target.after(phantom);
+          }
+        } else {                                                  // если мы над списком
+          const list = e.target;
+          const cards = list.querySelectorAll('.card');
+
+          if (cards.length > 0) {                                 // если в списке есть карточки
+            for (let i = 0; i < cards.length; i++) {
+              if (cards[i+1]) {                                     // если есть следующая карточка
+                if (cards[i].getBoundingClientRect().y < e.clientY && cards[i+1].getBoundingClientRect().y > e.clientY) {
+                  cards[i].after(phantom);                            // ставим фантом между ними
+                  break;
+                } else {                                            // если следующей карточки нет (мы на последней)
+                  if (cards[i].getBoundingClientRect().y < e.clientY) {     // если y текущей меньше, ставим после неё
+                    cards[i].after(phantom);
+                    break;
+                  }
+                  else {
+                    cards[i].before(phantom);                       // если y текущей больше, ставим до неё
+                    break;
+                  }
+                }
+              }
+            }
+          } else {                                                // если карточек нет
+            e.target.textContent = '';
+            delPhantom();
+            e.target.append(phantom);                               // вставляем фантом
+            e.target.classList.remove('bordered');
+          }
         }
+      } else {
+        if (current === e.target || current === dragged || current === phantom) {
+          return;
+        }
+        delPhantom();
+        if (e.target.classList.contains('card')) {                // если мы над карточкой
+          if (isUpperThenCenter(e)) {
+            e.target.before(phantom);
+          } else {
+            e.target.after(phantom);
+          }
+        } else {                                                  // если мы над списком
+          const list = e.target;
+          const cards = list.querySelectorAll('.card');
 
+          if (cards.length > 0) {                                 // если в списке есть карточки
+            for (let i = 0; i < cards.length; i++) {
+              if (cards[i+1]) {                                     // если есть следующая карточка
+                if (cards[i].getBoundingClientRect().y < e.clientY && cards[i+1].getBoundingClientRect().y > e.clientY) {
 
-
-
-        if (isUpperThenCenter(e)) {
-          e.target.before(phantom);
-        } else {
-          e.target.after(phantom);
+                  cards[i].after(phantom);                          // ставим фантом между ними
+                  break;
+                } 
+              } else {                                            // если следующей карточки нет (мы на последней)
+                if (cards[i].getBoundingClientRect().y < e.clientY) {     // если y текущей меньше, ставим после неё
+                  cards[i].after(phantom);
+                  break;
+                }
+                else {
+                  cards[i].before(phantom);                       // если y текущей больше, ставим до неё
+                  break;
+                }
+              }
+            }
+          } else {                                                // если карточек нет
+            e.target.textContent = '';
+            e.target.append(phantom);                               // вставляем фантом
+            e.target.classList.remove('bordered');
+          }
         }
       }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     })
 
     target.addEventListener("drop", (e) => {
-      
+      //delPhantom();
+
+      if (!(e.target.classList.contains('card') || e.target.classList.contains('cardList') || e.target.classList.contains('phantom'))) {
+        return;
+      }
+
       if (e.target.classList.contains('card')) {
         if (isUpperThenCenter(e)) {
           e.target.before(dragged);
         } else {
           e.target.after(dragged);
         }
-      } else {
+      } else if (e.target.classList.contains('cardList')) {
         const list = e.target;
         const cards = list.querySelectorAll('.card');
-        if (cards.length > 1) {
+
+        if (cards.length > 0) {                                 //если карточек больше 0
           for (let i = 0; i < cards.length; i++) {
-            if (cards[i].getBoundingClientRect().y < e.clientY && cards[i+1].getBoundingClientRect().y > e.clientY) {
-              cards[i].after(dragged);
-              break;
+            if (cards[i+1]) {                                   // если существует следующая карточка
+              if (cards[i].getBoundingClientRect().y < e.clientY && cards[i+1].getBoundingClientRect().y > e.clientY) { // если y текущей меньше, а следующей больше, ставим между ними
+                cards[i].after(dragged);
+                break;
+              }
+            } else {                                            // если следующей карточки нет (мы на последней)
+              if (cards[i].getBoundingClientRect().y < e.clientY) {     // если y текущей меньше, ставим после неё
+                cards[i].after(dragged);
+                break;
+              }
+              else {
+                cards[i].before(dragged);                       // если y текущей больше, ставим до неё
+                break;
+              }
             }
           }
-        } else {
+        } else {                                                // если карточек нет
           e.target.textContent = '';
           e.target.append(dragged);
           e.target.classList.remove('bordered');
         }
+      } else {
+        e.target.before(dragged);
+        delPhantom();
+
       }
     });
+
+    target.addEventListener("dragend", () => delPhantom());
   }
 }
 
